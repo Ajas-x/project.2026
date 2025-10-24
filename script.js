7b8e795dd796cba7a4b78b52d90b4996825573e1
Muhammed Ajas K <muhammadajas333@gmail.com>
2025-10-24 16:31:38 +0530

    v1.0.2

---
+++ script.js
@@ -0,0 +1,16 @@
+// function sampleFunction() {
+//     age = 20;
+//     if (age >= 19) {
+//         console.log("he is an adult");
+//         console.log("age:", age);
+//     }
+//     else if (age == 18) {
+//         console.log("age is exactly 18");
+//         console.log("age:", age);
+//     } else {
+//         console.log("age is less than 18");
+//         console.log("age:", age);
+//     }
+    
+// }
+// sampleFunction();
